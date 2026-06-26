9af8f77a060cb22e91e22660ba6991f9a2009914
Danny <x25174142@student.ncirl.ie>
2026-06-26 22:22:09 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/dannypierse/communitygardenmanager/CommunityGardenManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dannypierse.communitygardenmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityGardenManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityGardenManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dannypierse/communitygardenmanager/CommunityGardenManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannypierse.communitygardenmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityGardenManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
