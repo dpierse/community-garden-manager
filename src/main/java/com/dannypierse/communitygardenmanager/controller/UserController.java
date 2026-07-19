1094f027d3a9578145bc6285d9530e5f8b7b89ce
Danny <x25174142@student.ncirl.ie>
2026-07-19 18:46:35 +0100

    Implemented user management backend structure

---
+++ src/main/java/com/dannypierse/communitygardenmanager/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.dannypierse.communitygardenmanager.controller;
+
+import com.dannypierse.communitygardenmanager.model.User;
+import com.dannypierse.communitygardenmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/test")
+    public String test() {
+        return "Controller is working";
+    }
+    @GetMapping("/create-test-user")
+    public String createTestUser() {
+
+        User user = new User();
+        user.setName("Danny");
+        user.setEmail("danny@test.com");
+        user.setPassword("1234");
+        user.setRole("USER");
+
+        userService.saveUser(user);
+
+        return "User created successfully";
+    }
+}
---
+++ src/main/java/com/dannypierse/communitygardenmanager/service/UserService.java
@@ -0,0 +1,28 @@
+package com.dannypierse.communitygardenmanager.service;
+
+import com.dannypierse.communitygardenmanager.model.User;
+import com.dannypierse.communitygardenmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
